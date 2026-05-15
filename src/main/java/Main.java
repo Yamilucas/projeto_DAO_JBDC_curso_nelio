4fae2caf924fb7b0dade5d930b6bbc31042bde40
Lucas Eufrásio <lucas_hyan@yahoo.com.br>
2026-05-15 13:13:03 -0300

    Entidade da classe de Departamento arrumada

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,33 +1,9 @@
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
-import db.DB;
-import db.DbIntegrityException;
+import model.entities.Department;
 
 public class Main {
     public static void main(String[] args) {
 
-        Connection conn = null;
-        PreparedStatement st = null;
-        try {
-            conn = DB.getConnection();
-
-            st = conn.prepareStatement(
-                    "DELETE FROM department "
-                            + "WHERE "
-                            + "Id = ?");
-
-            st.setInt(1, 5);
-
-            int rowsAffected = st.executeUpdate();
-
-            System.out.println("Done! Rows affected: " + rowsAffected);
-        } catch (SQLException e) {
-            throw new DbIntegrityException(e.getMessage());
-        } finally {
-            DB.closeStatement(st);
-            DB.closeConnection();
-        }
+        Department obj = new Department(1,"Books");
+        System.out.println(obj);
     }
 }
