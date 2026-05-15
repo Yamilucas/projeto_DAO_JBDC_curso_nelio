0e193cad2ad735bbba942bc8defaa31436ba490d
Lucas Eufrásio <lucas_hyan@yahoo.com.br>
2026-05-15 16:12:56 -0300

    Adicionado a implementação do findById

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,15 @@
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
-
-import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
 
-        Department obj = new Department(1,"Books");
-        System.out.println(obj);
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
-        Seller seller = new Seller(21,"Bob","bob@gmail.com", LocalDate.now(), 3000.0, obj);
+        Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
 
-        SellerDao sellerDao = DaoFactory.createSellerDao();
     }
 }
