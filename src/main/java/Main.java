4b202fcdef547223187058dca6ce2ce1189987a3
Lucas Eufrásio <lucas_hyan@yahoo.com.br>
2026-05-16 15:40:46 -0300

    Implementando o update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -29,10 +29,17 @@ public class Main {
             System.out.println(obj);
         }
 
-        System.out.println("\n=== TEST 4: seller insert =====");
+        System.out.println("\n=== TEST :4 seller insert =====");
         Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", Date.valueOf("2024-05-06"), 4000.0, department);
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
+
+        System.out.println("\n=== TEST :5 seller update =====");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Wayne");
+        sellerDao.update(seller);
+        System.out.println("Updated completed");
+
 
 
     }
