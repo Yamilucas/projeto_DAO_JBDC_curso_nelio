9d3aaf7bb1aab53673cc67135fd052d331589d0a
Lucas Eufrásio <lucas_hyan@yahoo.com.br>
2026-05-16 16:12:36 -0300

    Implementando o delete

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,9 +5,11 @@ import model.entities.Seller;
 
 import java.sql.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
@@ -40,6 +42,13 @@ public class Main {
         sellerDao.update(seller);
         System.out.println("Updated completed");
 
+        System.out.println("\n=== TEST :6 seller delete =====");
+        System.out.println("Enter id for delete test :");
+        int id = sc.nextInt();
+        sellerDao.deleteById(id);
+        System.out.println("Delete completed");
+
+        sc.close();
 
 
     }
